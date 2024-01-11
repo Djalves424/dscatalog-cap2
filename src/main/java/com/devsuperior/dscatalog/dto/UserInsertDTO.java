215e5c6aba1f19376c24e8424500bbfafbde9f44
Diego de Jesus <djalves424@gmail.com>
2024-01-11 17:16:25 -0300

    User CRUD

--- src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,11 +1,12 @@
 package com.devsuperior.dscatalog.dto;
 
 public class UserInsertDTO extends UserDTO {
+    private static final long serialVersionUID = 1L;
 
     private String password;
 
     public UserInsertDTO() {
-
+        super();
     }
 
     public String getPassword() {
--- src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,15 +1,5 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Category;
@@ -18,8 +8,16 @@ import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
-
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
