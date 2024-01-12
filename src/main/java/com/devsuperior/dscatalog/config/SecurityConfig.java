e1f63d451744e19a92ba4459bf821daca3a27099
Diego de Jesus <djalves424@gmail.com>
2024-01-12 17:15:35 -0300

    Fixing SecurityConfig class

--- src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
 }
