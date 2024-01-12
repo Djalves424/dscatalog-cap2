6245bcf97fb00baee712ea68d19792ee651b0b16
djalves424 <djalves424@gmail.com>
2024-01-11 23:33:51 -0300

    Fixing the configuration class

--- src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
